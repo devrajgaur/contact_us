2d8b0840fcd91292d5e28ac3dec8d6b74463dd50
Dev Raj Gaur <devrajgaur786@gmail.com>
2021-08-08 16:40:47 +0530

    learned about Scaffold and hot reload in this lesson

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Center(
-          child: Text("Hello World"),
+void main() {
+  runApp(contact());
+}
+
+class contact extends StatelessWidget {
+  const contact({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[200],
+        appBar: AppBar(
+          title: Text("Contact Us"),
+          backgroundColor: Colors.green,
         ),
+        body: Text("Hello World"),
       ),
     );
+  }
+}
