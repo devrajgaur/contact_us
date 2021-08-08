bccb70a0fec13d7e50cf6222676ed2fe0ca6874d
Dev Raj Gaur <devrajgaur786@gmail.com>
2021-08-09 01:02:35 +0530

    Learned to add columns, rows, adding images, aligning images, containers, padding, etc

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,68 @@ class contact extends StatelessWidget {
           backgroundColor: Colors.white,
         ),
         body: Column(
-          children: <Widget>[Image.asset('images/contact.jpg')],
+          children: <Widget>[
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: Image.asset(
+                'images/contact.jpg',
+                height: 250,
+              ),
+            ),
+            Text(
+              'If you need help, \n Feel free to contact us',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, color: Colors.grey[800]),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    color: Colors.orange,
+                    child: Text('Container 1'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    color: Colors.orange,
+                    child: Text('Container 1'),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    color: Colors.orange,
+                    child: Text('Container 1'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    color: Colors.orange,
+                    child: Text('Container 1'),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
